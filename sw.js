56b0dbd2ee1d0cc377caebc8118043c6d2ef01c3
chixxchica <chi.ke.323.715@gmail.com>
2026-09-23 21:01:03 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='youth-baseball-team-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{
+      const clone=resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return resp;
+    }).catch(()=>cached))
+  );
+});
